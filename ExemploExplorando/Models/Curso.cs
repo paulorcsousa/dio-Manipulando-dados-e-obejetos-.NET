97b487bd83dac610a3d34af0b0b08b14be7357be
paulorcsousa <103280898+paulorcsousa@users.noreply.github.com>
2024-05-08 10:00:11 -0300

    aula 1 unidade 3
    Unidade Manipulando dados e objetos com .NET
    Aula: Propriedades, Métodos e Construtores com C#.

---
+++ ExemploExplorando/Models/Curso.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploExplorando.Models
+{
+    public class Curso
+    {
+        public string Nome { get; set; }
+        public List<Pessoa> Alunos { get; set; }
+
+        public void AdicionarAluno(Pessoa Aluno)
+        {
+            Alunos.Add(Aluno);
+        }
+
+        public int ObterQuantidadeDeAlunosMatriculados()
+        {
+            int quantidade = Alunos.Count;
+            return quantidade; 
+        }
+
+        public bool RemoverAluno(Pessoa aluno)
+        {
+            return Alunos.Remove(aluno);
+        }
+
+        public void ListarAlunos()
+        {
+            Console.WriteLine($"Alunos do curso {Nome}");
+            foreach(Pessoa aluno in Alunos)
+            {
+                Console.WriteLine(aluno.NomeCompleto);
+            }
+        }
+
+    }
+}
---
+++ ExemploExplorando/Program.cs
@@ -0,0 +1,28 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa(nome: "Paulo",sobrenome: "Renan");
+Pessoa p2 = new Pessoa(nome: "Renan",sobrenome: "Silva");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Paulo";
+// p1.Sobrenome = "Renan";
+// p1.Idade = 36;
+// p1.Apresentar();
